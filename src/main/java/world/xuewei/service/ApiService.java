4014e2318e475b8f13a14217b0df229c256ab29f
Mike <1400034243@qq.com>
2026-03-25 09:57:06 +0800

    3月25日

--- src/main/java/world/xuewei/service/ApiService.java
+++ src/main/java/world/xuewei/service/ApiService.java
@@ -13,20 +13,6 @@ import org.springframework.stereotype.Service;
 
 /**
  * 智慧医生服务
- * <p>
- * ==========================================================================
- * 郑重说明：本项目免费开源！原创作者为：薛伟同学，严禁私自出售。
- * ==========================================================================
- * B站账号：薛伟同学
- * 微信公众号：薛伟同学
- * 作者博客：http://xuewei.world
- * ==========================================================================
- * 陆陆续续总会收到粉丝的提醒，总会有些人为了赚取利益倒卖我的开源项目。
- * 不乏有粉丝朋友出现钱付过去，那边只把代码发给他就跑路的，最后还是根据线索找到我。。
- * 希望各位朋友擦亮慧眼，谨防上当受骗！
- * ==========================================================================
- *
- * @author <a href="http://xuewei.world/about">XUEW</a>
  */
 @Service
 public class ApiService {
@@ -39,7 +25,7 @@ public class ApiService {
         try {
             Generation gen = new Generation();
             MessageManager msgManager = new MessageManager(10);
-            Message systemMsg = Message.builder().role(Role.SYSTEM.getValue()).content("你是薛伟同学开发的智能医生，你只回答与医疗相关的问题，不要回答其他问题！").build();
+            Message systemMsg = Message.builder().role(Role.SYSTEM.getValue()).content("你是智能医疗助手，只回答与医疗相关的问题，不要回答其他问题。").build();
             Message userMsg = Message.builder().role(Role.USER.getValue()).content(queryMessage).build();
             msgManager.add(systemMsg);
             msgManager.add(userMsg);
